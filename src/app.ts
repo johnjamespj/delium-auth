1944556b0f52fc9594c5e560b02118d424832c12
John James <johnjamespj@gmail.com>
2021-07-24 22:58:55 +0000

    Added a blueprint for the service

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+/**
+ * Delium Auth Service: A service that manages users authentication.
+ * 
+ *      This service creates a single users, edit user, create access tokens, revoke access
+ * tokens, edit user and delete user. This services includes a a cote publisher and responder
+ * that can be used to use the service. For the data storage, a "repo" abstraction is made so
+ * that any type of data storage can be used, with mongodb as the default data store.
+ * 
+ *  # Create a user
+ *  # Edit a user
+ *  # Recover account
+ *  # Create access tokens
+ *  # Revoke access tokens
+ *  # Edit a user
+ *  # Delete a user
+ */
--- src/sum.ts
+++
@@ -1,6 +0,0 @@
-export function sum(a?: number, b?: number) : number {
-    if (a && b){
-        return a + b;
-    }
-    return 0;
-}
